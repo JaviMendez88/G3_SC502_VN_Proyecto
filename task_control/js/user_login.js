c9ef5791a233d27afacd666f0a60c14c45372719
wen2494 <wmiranda00058@ufide.ac.cr>
2025-07-10 17:19:01 -0600

    Update user_login.js

--- task_control/js/user_login.js
+++ task_control/js/user_login.js
@@ -1,4 +1,17 @@
-  document.getElementById('loginForm').addEventListener('submit', function(event) {
+document.getElementById('loginForm').addEventListener('submit', function(event) {
     event.preventDefault();
-    window.location.href = 'userprofile_main.html';
-  });
+
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    const usuarios = JSON.parse(localStorage.getItem('usuarios')) || [];
+
+    const usuarioValido = usuarios.find(usuario => usuario.email === email && usuario.password === password);
+
+    if (usuarioValido) {
+        sessionStorage.setItem('usuarioActivo', email);
+        window.location.href = 'userProfile_main.html';
+    } else {
+        alert('Correo o contraseña incorrectos.');
+    }
+});
